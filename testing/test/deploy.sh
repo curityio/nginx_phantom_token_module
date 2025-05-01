ca7f38ffc524475c03bf93d1d96d015cdd38fb70
gary.archer <gary.archer@curity.io>
2025-05-01 09:57:29 +0200

    Minor updates for productive developer testing

---
+++ testing/test/deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+ADMIN_PASSWORD=Password1
+
+#
+# Prompt if required
+#
+if [ "$LICENSE_FILE_PATH" == '' ]; then
+  read -t 10 -p 'Enter the path to the license file for the Curity Identity Server: ' LICENSE_FILE_PATH || :
+fi
+LICENSE_FILE_PATH=$(eval echo "$LICENSE_FILE_PATH")
+
+#
+# Check we have valid data before proceeding
+#
+if [ ! -f "$LICENSE_FILE_PATH" ]; then
+  >&2 echo 'A valid LICENSE_FILE_PATH parameter was not supplied'
+  exit 1
+fi
+LICENSE_KEY=$(cat "$LICENSE_FILE_PATH" | jq -r .License)
+if [ "$LICENSE_KEY" == '' ]; then
+  >&2 echo 'A valid license key was not found'
+  exit 1 
+fi
+
+#
+# Deploy the system
+#
+export LICENSE_KEY
+export ADMIN_PASSWORD
+docker compose down
+docker compose up -d
+
+#
+# Wait for the Identity Server to come up
+#
+echo 'Waiting for the Curity Identity Server to start...'
+c=0; while [[ $c -lt 25 && "$(curl -fs -w ''%{http_code}'' localhost:8443)" != "404" ]]; do ((c++)); echo -n "."; sleep 2; done
+echo
